0fa7e34568f8f1d118ce05d951d66e379c05aa22
Artem Paliutin <pal.artem@icloud.com>
2022-04-28 23:14:48 +0300

    Initial Commit

---
+++ CaloriesDemo/CaloriesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesDemoApp.swift
+//  CaloriesDemo
+//
+//  Created by Artem Paliutin on 28/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
